95abbaf820b234948b77a25d01180114988033a5
Aditya Yadav <ramanandyadav7650@gmail.com>
2023-09-26 15:41:05 +0530

    update

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 var show_button = document.getElementById("show_btn");
 var links_cont = document.getElementById("links");
 var close_btn = document.getElementById("close");
-
-
-show_button.addEventListener("click", function(){
-    show();
-});
-close_btn.addEventListener("click", function(){
-    hide();
-})
+var close_links = document.getElementById("hide");
+var close_links1 = document.getElementById("hide1");
+var close_links2 = document.getElementById("hide2");
+var close_links3 = document.getElementById("hide3");
+var close_links4 = document.getElementById("hide4");
+var close_links5 = document.getElementById("hide5");
 
 
 const show = () => {
@@ -18,3 +16,29 @@ const show = () => {
 const hide = () => {
     links_cont.style.marginTop = "-265px";
 }
+
+
+show_button.addEventListener("click", function(){
+    show();
+});
+close_btn.addEventListener("click", function(){
+    hide();
+})
+close_links.addEventListener("click", function(){
+    hide();
+})
+close_links1.addEventListener("click", function(){
+    hide();
+})
+close_links2.addEventListener("click", function(){
+    hide();
+})
+close_links3.addEventListener("click", function(){
+    hide();
+})
+close_links4.addEventListener("click", function(){
+    hide();
+})
+close_links5.addEventListener("click", function(){
+    hide();
+})
